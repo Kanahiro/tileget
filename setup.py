76c7a09cf352a554cb42830d70a555732f159f04
Kanahiro <mediterranean1769@gmail.com>
2022-11-27 14:33:07 +0900

    0.1.0

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ def _requires_from_file(filename):
 
 setup(
     name="tileget",
-    version="0.0.2",
+    version="0.1.0",
     description="Tile download utility - easily download xyz-tile data",
     author="Kanahiro Iguchi",
     license="MIT",
     url="https://github.com/Kanahiro/tileget",
     packages=find_packages(),
-    install_requires=_requires_from_file('requirements.txt'),
+    install_requires=_requires_from_file("requirements.txt"),
     entry_points={
         "console_scripts": [
             "tileget=tileget.__main__:main",
         ]
-    }
+    },
 )
